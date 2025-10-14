671f8efbc73f2260fc3d120bb50e0d941a06d2fb
vidushisaini21 <vidushis2003@gamil.com>
2025-10-14 15:05:15 +0530

    new

---
+++ javascript/ES6/DOM.js
@@ -0,0 +1,6 @@
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+h1.innerText="python is create AI";
+div.appendChild(h1);
+const id=document.getElementById("container");
+id.appendChild(div);
