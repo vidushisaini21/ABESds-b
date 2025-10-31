931656563df54a120f6825045cef3464b4d81063
vidushisaini21 <vidushis2003@gamil.com>
2025-10-31 15:41:51 +0530

    react

---
+++ frontend/react/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+function Card() {
+  return (
+    <div id='card'>
+      <img src="https://img.freepik.com/free-vector/boy-happy-childhood-icon-isolated_24640-134167.jpg?semt=ais_hybrid&w=740&q=80" alt="" height={100} width={100}/>
+      <h1>ABES</h1>
+      <h1>BTECH</h1>
+    </div>
+  )
+}
+
+export default Card
---
+++ frontend/react/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  './Nav.css'
+function Nav() {
+  return (
+    <div>
+      <div id='Navbar'>
+        <div id='container'> <h1>HOME</h1></div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Nav
