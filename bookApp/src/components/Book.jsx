5e01c23098c781c61c46f616a3e3ae6177db9186
vidushisaini21 <vidushis2003@gamil.com>
2025-11-07 13:59:39 +0530

    check

--- bookApp/src/components/Book.jsx
+++ bookApp/src/components/Book.jsx
@@ -6,6 +6,7 @@ function Book(props) {
     <img  src="https://i.pinimg.com/736x/ff/1e/4d/ff1e4d2a0fc1e39f4ef5303e9108b269.jpg" alt="" height={100} width={100} dis/>
       <h1 >Title:{props.title}</h1>
       <h1 >Price:{props.price}</h1>
+      <div></div>
     </div>
   )
 }
